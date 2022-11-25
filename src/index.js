e33532ed690b872180ddb292ad6a667ca26358fa
Surpreet Kaur <gni.surpreet@gmail.com>
2022-11-25 11:06:47 +0530

    useState form revised

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import EvenOdd from './useState';
+import DataForm from './useState';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    {/* <EvenOdd /> */}
+    {/* <App /> */}
+    <DataForm />
   </React.StrictMode>
 );
 
--- src/useState.js
+++ src/useState.js
@@ -2,29 +2,134 @@ import React, { useState } from "react";
 import "./index.css";
 
 
-const EvenOdd = () => {
-  const [categories, setCategories] = React.useState([]);
-  const [item, setItem] = React.useState("");
+const DataForm = () => {
+  const [edit, setEdit] = useState(false);
+  const [detail, setDetail] = useState([]);
+  const [record, setRecord] = useState({
+      name:"",
+      email:""
+  });
 
-  const addCategory = e => { 
-    const trimmed = item.trim();
-
-    if (trimmed && !categories.includes(trimmed)) {
-      setCategories(prevState => prevState.concat(trimmed));
+  const onChanged = (e) => {
+    setRecord({
+      ...record,
+      [e.target.name] : e.target.value
     }
+    )
+    //console.log(e.target.value)
   };
 
+  const saveRecord = (e) => {
+    e.preventDefault();
+    const arrIndex = detail.findIndex((item) => item.email === record.email);
+    if(arrIndex === -1){
+    setRecord({
+      name:"",
+      email:""
+    });
+    setDetail([...detail, record]);
+    }else{
+      alert("Email should not be repeated!")
+    }
+    console.log("detail", detail); 
+  };  
+
+  const updateRecord = (e) => {
+    const newUpdate = [...detail]
+    e.preventDefault();
+    const arrIndex = detail.findIndex((item) => item.email === record.email);
+    if(arrIndex === -1){
+    return ;
+    }else{
+    newUpdate[arrIndex].name = record.name;
+    setDetail(newUpdate);
+    setRecord({
+      name:"",
+      email:""
+    });
+    setEdit(false);
+    }
+    console.log("detail", detail); 
+  };  
+
+  const deleted = (index, e) => {
+    const checkFilter = (item, i) => i !== index;
+    setDetail(
+      detail.filter(checkFilter)
+    )
+    setEdit(false);
+    console.log("index", index);
+  }
+
+  const edited = (item) => {
+    console.log("edited");
+    setEdit(true);
+    setRecord(item);
+  }
+
   return (
-    <div>
-      <input
-        onChange={e => setItem(e.target.value)}
-        value={item}
-      />&nbsp;
-      <button onClick={addCategory}>Add Item</button>
-      <ul>{categories.map(e => <li key={e}>{e}</li>)}</ul>
-    </div>
+    <>
+      <div className="margin-auto form-div">
+        <form onSubmit={edit ? updateRecord : saveRecord}>
+          <h3>Fill the form</h3>
+          <label>Name: </label>
+          <input
+            type="text"
+            name="name"
+            value={record.name} 
+            onChange={onChanged}
+            required
+          />
+          <br />
+          <br />
+          <label>Email: </label>
+          <input
+            type="email"
+            name="email"
+            value={record.email}
+            onChange={onChanged}
+            required
+            disabled={edit}
+          />
+          <br />
+          <br />
+          {edit ? <button>Update</button>  : <button>Submit</button>}
+        </form>
+      </div>
+      <div className="margin-auto">
+        <table width="100%">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {detail.map((item, index) => {
+              return(
+              <tr key={index}>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>
+                <button onClick={() => edited(item)}>
+                  Edit
+                </button>
+              </td>
+              <td>
+                <button onClick={(e) => deleted(index, e)}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+            )})} 
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
 
-export default EvenOdd;
+export default DataForm;
